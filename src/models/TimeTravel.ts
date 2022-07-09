8b02743ed5292a10b1ea69fcc7f51f88fdd3472d
jeonghoon <shb03323@naver.com>
2022-07-10 02:18:46 +0900

    [Feat] add image in TimeTravelSchema
    add image in TimeTravelSchema

--- src/models/TimeTravel.ts
+++ src/models/TimeTravel.ts
@@ -11,7 +11,12 @@ const TimeTravelSchema = new mongoose.Schema(
     title: {
         type: String,
         required: true
-    }
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    
   },
   {
     timestamps: true,
