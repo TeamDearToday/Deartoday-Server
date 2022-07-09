115248039c26a0667486a7b12946e00cf3d7fc38
jeonghoon <shb03323@naver.com>
2022-07-10 02:11:04 +0900

    [Feat] add timestampts in TimeTravelSchema

---
+++ src/models/TimeTravel.ts
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+import { TimeTravelInfo } from "../interfaces/timeTravel/TimeTravelInfo";
+
+const TimeTravelSchema = new mongoose.Schema(
+  {
+
+  },
+  {
+    timestamps: true,
+  },
+);
