6c06433e796490fcd58aa9ab3e7897207feab7c6
heerucan <iwpow0307@gmail.com>
2022-07-12 01:22:45 +0900

    [Add] #10 - Add Router

---
+++ src/routes/TimeTravelRouter.ts
@@ -0,0 +1,27 @@
+import { Router } from 'express';
+import TimeTravelController from '../controllers/timeTravel/TimeTravelController';
+
+const router: Router = Router();
+
+// 메인 - 시간여행 개수
+router.get('/count', TimeTravelController.getTimeTravelCount);
+
+// 시간여행 가상공간 - 특정 연도 사진 가져오기
+router.get('/oldMedia', TimeTravelController.getOldMedia);
+
+// 시간여행 가상공간 - 질문 가져오기
+router.get('/question', TimeTravelController.getQuestion);
+
+// 시간여행 가상공간 - 질문, 대답, 과거사진, 날짜, 제목 쓰기
+router.post('/', TimeTravelController.postTimeTravel);
+
+// 메세지 확인하기
+router.get('/answers', TimeTravelController.getAnswers);
+
+// 비디오 테이프 목록 가져오기
+router.get('/', TimeTravelController.getTimeTravelList);
+
+// 시간여행 상세내역 확안하기
+router.get('/:timeTravleId', TimeTravelController.getTimeTravelId);
+
+export default router;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,10 @@
 import { Router } from 'express';
+import TimeTravelRouter from './TimeTravelRouter';
+import AuthRouter from './AuthRouter';
 
 const router: Router = Router();
+
+router.use('/timetravel', TimeTravelRouter);
+router.use('/auth', AuthRouter);
 
 export default router;
