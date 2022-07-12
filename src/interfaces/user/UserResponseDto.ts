41439227390f6608d7dbd21dd527261883f86bf7
jeonghoon <shb03323@naver.com>
2022-07-13 00:48:29 +0900

    [Del] #15 - delete UserResponseDto

--- src/interfaces/user/UserResponseDto.ts
+++
@@ -1,6 +0,0 @@
-import mongoose from 'mongoose';
-import { UserCreateDto } from './UserCreateDto';
-
-export interface UserResponseDto extends UserCreateDto {
-  _id: mongoose.Schema.Types.ObjectId;
-}
