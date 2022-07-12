038866a05d6ab5a0c1cbac95d3aeefa92d4680e1
jeonghoon <shb03323@naver.com>
2022-07-12 11:55:29 +0900

    [Add] add accessToken in UserSchema

--- src/models/User.ts
+++ src/models/User.ts
@@ -7,6 +7,10 @@ const UserSchema = new mongoose.Schema({
         enum: ['kakao', 'apple'],
         required: true
     },
+    accessToken: {
+        type: String,
+        required: true
+    },
     fcmTokens: {
         type: [String],
         required: true
