f8d95bf265c1e840814d7d34dcd976a37add98cd
jeonghoon <shb03323@naver.com>
2022-07-13 22:04:25 +0900

    [Add] #20 - add import in TimeTravelService

--- src/services/timetravel/TimeTravelService.ts
+++ src/services/timetravel/TimeTravelService.ts
@@ -1,2 +1,7 @@
+import mongoose from "mongoose";
+import { PostBaseResponseDto } from "../../interfaces/common/PostBaseResponseDto";
+import { TimeTravelCountDto } from "../../interfaces/timeTravel/TimeTravelCountDto";
+import TimeTravel from "../../models/TimeTravel";
+
 const TimeTravelService = {};
 export default TimeTravelService;
