17a8ddae13847c08033e9266fc3b758b42cb11c9
jeonghoon <shb03323@naver.com>
2022-07-13 12:36:42 +0900

    [Add] #15 - add email field in UserSchema

--- src/interfaces/user/UserInfo.ts
+++ src/interfaces/user/UserInfo.ts
@@ -1,6 +1,6 @@
 export interface UserInfo {
   socialType: string;
-  email; string;
+  email: string;
   accessToken: string;
   fcmTokens: string[];
 }
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,20 +1,25 @@
-import mongoose from "mongoose";
-import { UserInfo } from "../interfaces/user/UserInfo";
+import mongoose from 'mongoose';
+import { UserInfo } from '../interfaces/user/UserInfo';
 
 const UserSchema = new mongoose.Schema({
-    socialType: {
-        type: String,
-        enum: ['KAKAO', 'APPLE'],
-        required: true
-    },
-    accessToken: {
-        type: String,
-        required: true
-    },
-    fcmTokens: {
-        type: [String],
-        required: true
-    }
-})
+  socialType: {
+    type: String,
+    enum: ['KAKAO', 'APPLE'],
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  accessToken: {
+    type: String,
+    required: true,
+  },
+  fcmTokens: {
+    type: [String],
+    required: true,
+  },
+});
 
 export default mongoose.model<UserInfo & mongoose.Document>('User', UserSchema);
