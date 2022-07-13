7641aa40267f20fdff193d149f69ce5f455c1f25
jeonghoon <shb03323@naver.com>
2022-07-14 00:43:45 +0900

    [Add] #23 - add error case in getQuestion controller

--- src/controllers/timeTravel/TimeTravelController.ts
+++ src/controllers/timeTravel/TimeTravelController.ts
@@ -44,6 +44,9 @@ const getQuestion = async (req: Request, res: Response) => {
     if (!data) res.status(statusCode.NOT_FOUND).send(util.fail(statusCode.NOT_FOUND, message.NOT_FOUND));
 
     res.status(statusCode.OK).send(util.success(statusCode.OK, message.GET_QUESTIONS_SUCCESS, data));
+  } catch (error) {
+    console.log(error);
+    res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, message.INTERNAL_SERVER_ERROR));
   }
 };
 
