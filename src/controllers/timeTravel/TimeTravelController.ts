914e06f68a5c2337a55ca2af97cf8cd25f45d971
jeonghoon <shb03323@naver.com>
2022-07-14 00:39:23 +0900

    [Add] #23 - add success case in getQuestion controller

--- src/controllers/timeTravel/TimeTravelController.ts
+++ src/controllers/timeTravel/TimeTravelController.ts
@@ -37,7 +37,15 @@ const getOldMedia = async (req: Request, res: Response) => {};
  *  @access Public
  */
 
-const getQuestion = async (req: Request, res: Response) => {};
+const getQuestion = async (req: Request, res: Response) => {
+  const userId = req.body.userId;
+  try {
+    const data = await TimeTravelService.getQuestion(userId);
+    if (!data) res.status(statusCode.NOT_FOUND).send(util.fail(statusCode.NOT_FOUND, message.NOT_FOUND));
+
+    res.status(statusCode.OK).send(util.success(statusCode.OK, message.GET_QUESTIONS_SUCCESS, data));
+  }
+};
 
 /**
  *  @route Get /answers
