15be731b62ca597fcfe82c9c8d05e7818fa22798
jeonghoon <shb03323@naver.com>
2022-07-14 23:56:34 +0900

    [Add] #29 - add logout router

--- src/routes/AuthRouter.ts
+++ src/routes/AuthRouter.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import AuthContoller from '../controllers/auth/AuthController';
+import auth from '../middlewares/auth';
 
 const router: Router = Router();
 
 router.post('/login/:social', AuthContoller.socialLogin);
-router.patch('/logout');
+router.patch('/logout', auth, AuthContoller.socialLogout);
 
 export default router;
