dc9ccc5784b386faeba29f2c997c0870c100d47e
heerucan <iwpow0307@gmail.com>
2022-07-18 03:00:32 +0900

    [Feat] #24 - postTimeTravel api 오류 수정

--- src/services/timetravel/TimeTravelService.ts
+++ src/services/timetravel/TimeTravelService.ts
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
-import { TimeTravelCreateDto } from '../../interfaces/timeTravel/TimeTravelCreateDto';
+import { TimeTravelInfo } from '../../interfaces/timeTravel/TimeTravelInfo';
 import { GetQuestionDto } from '../../interfaces/timeTravel/GetQuestionDto';
 import { GetTimeTravelDto } from '../../interfaces/timeTravel/GetTimeTravelAllDto';
 import { TimeTravelCountDto } from '../../interfaces/timeTravel/TimeTravelCountDto';
@@ -9,11 +9,67 @@ import getRandomQuestions from '../../modules/shuffleQuestion';
 import { GetTimeTravelDetailDto } from '../../interfaces/timeTravel/GetTimeTravelDetailDto';
 import { Result } from 'express-validator';
 import { GetAnswersDto } from '../../interfaces/timeTravel/GetAnswersDto';
+import { TimeTravelCreateDto } from '../../interfaces/timeTravel/TimeTravelCreateDto';
+import { MessageInfo } from '../../interfaces/message/MessageInfo';
+import Message from '../../models/Message';
 
-const postTimeTravel = async (timeTravelCreateDto: TimeTravelCreateDto) => {
+const postTimeTravel = async (timeTravelCreateDto: TimeTravelCreateDto): Promise<TimeTravelInfo> => {
   try {
-    const timeTravel = new TimeTravel(timeTravelCreateDto);
+    const timeTravelQuestions = timeTravelCreateDto.questions;
+    const timeTravelAnswers = timeTravelCreateDto.answers;
+
+    // /**
+    //  * TODO
+    //  * 메시지 저장 promise로 하는 방법 생각해보기
+    //  * Post 할 때 배열로 넣는 방법 찾아내서 배열로 넣기
+    //  * timeTravelCreateDto 말고 timeTravelInfo로 저장하기 +
+    //  * TimeTravel에 저장하기 전에 timeTravelInfo에 우리가 저장한 메시지들을 timeTravelInfo 요소로 넣기 +
+    //  * timeTravel 저장 +
+    //  */
+
+    let messageList: MessageInfo[] = [];
+
+    for (let i = 0; i < timeTravelQuestions.length; i++) {
+      const message = new Message({
+        question: timeTravelQuestions[i],
+        answer: timeTravelAnswers[i],
+      });
+
+      await message.save();
+      messageList.push(message._id);
+    }
+
+    // const data = await Promise.all(
+    //   messageList.map(async (message) => {
+    //     const dataList = new Message({
+    //       question: message.question,
+    //       answer: message.answer,
+    //     });
+
+    //     await dataList.save();
+
+    //     // objectid
+    //     const messageInfo = {
+    //       _id: dataList._id,
+    //     };
+    //   }),
+    // );
+
+    // console.log(data, '데이터소연?');
+
+    const timeTravel = new TimeTravel({
+      userId: timeTravelCreateDto.userId,
+      image: timeTravelCreateDto.image,
+      title: timeTravelCreateDto.title,
+      year: timeTravelCreateDto.year,
+      month: timeTravelCreateDto.month,
+      day: timeTravelCreateDto.day,
+      writtenDate: timeTravelCreateDto.writtenDate,
+      messages: messageList,
+    });
     await timeTravel.save();
+
+    console.log(timeTravel);
     return timeTravel;
   } catch (error) {
     console.log(error);
@@ -60,17 +116,20 @@ const getAnswers = async (userId: string): Promise<GetAnswersDto[] | null> => {
     if (!user) {
       return null;
     }
-    const answers = await TimeTravel.find(
-      answers: answer
+
+    const timeTravels = await TimeTravel.find({
+      user: userId,
+    }).populate('messages', 'answer');
+
+    const data = await Promise.all(
+      timeTravels.map(async (timeTravel) => {
+        const result: GetAnswersDto = {
+          lastAnswer: timeTravel.messages[6].answer,
+        };
+
+        return result.lastAnswer;
+      }),
     );
-
-    console.log(answers);
-
-    const data: GetAnswersDto[] = {
-      lastAnswer: answers,
-    };
-
-    console.log(answers, data);
 
     return data;
   } catch (error) {
