19ad56502a0c72804519ecf8c01be721fffe35d1
jeonghoon <shb03323@naver.com>
2022-07-19 04:06:49 +0900

    [Add] #35 - add login success test

--- src/test/auth.spec.ts
+++ src/test/auth.spec.ts
@@ -1,12 +1,27 @@
 import router from '../routes/index';
-import request from 'supertest';
 import { expect } from 'chai';
+import app from '../index';
 import dotenv from 'dotenv';
+import req from 'supertest';
 dotenv.config();
 
 describe('POST /auth/login/KAKAO', () => {
-    it('카카오 로그인 성공', done => {
-        request(router)
-        .get('/auth/login')
-    })
-})
+  it('카카오 로그인 성공', (done) => {
+    req(app)
+      .post('/auth/login/KAKAO')
+      .set('Content-Type', 'application/json')
+      .send({
+        socialToken: process.env.KAKAO_TOKEN,
+        fcmToken: process.env.FCM_TOKEN,
+      })
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .then((res) => {
+        done();
+      })
+      .catch((err) => {
+        console.error('######Error >>', err);
+        done(err);
+      });
+  });
+});
