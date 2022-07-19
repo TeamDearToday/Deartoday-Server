848b8985ffe05509fdee4e8f9ee7f749fadbe691
jeonghoon <shb03323@naver.com>
2022-07-19 16:30:57 +0900

    [Bug] #46 - json type을 바꾸어줘야함

--- src/test/timeTravel.spec.ts
+++ src/test/timeTravel.spec.ts
@@ -9,7 +9,7 @@ dotenv.config();
  * 200, 401 케이스
  */
 describe('GET /timeTravel/count', () => {
-    // 시간여행 개수 조회 성공 케이스
+  // 시간여행 개수 조회 성공 케이스
   it('시간여행 개수 조회 - 성공', (done) => {
     req(app)
       .get('/timeTravel/count')
@@ -47,3 +47,30 @@ describe('GET /timeTravel/count', () => {
  * 시간여행 생성
  * 201, 400 케이스
  */
+describe('POST /timeTravel', () => {
+  // 시간여행 생성 성공 케이스
+  it('시간여행 생성 - 성공', (done) => {
+    req(app)
+      .post('/timeTravel')
+      .set('Content-Type', 'multipart/form-data')
+      .set({ Authorization: `Bearer ${process.env.TEST_TOKEN}` })
+      .send({
+        title: '김루희 똥방구',
+        image: 'https://cdn.pixabay.com/photo/2022/04/29/17/48/lofoten-7164179_1280.jpg',
+        year: 2022,
+        month: 7,
+        day: 19,
+        currentDate: '2022.07.20',
+        questions: ['질문1', '질문2', '질문3', '질문4', '질문5', '질문6', '질문7'],
+        answers: ['대답1', '대답2', '대답3', '대답4', '대답5', '대답6', '대답7'],
+      })
+      .expect(201)
+      .then((res) => {
+        done();
+      })
+      .catch((err) => {
+        console.error('######Error >>', err);
+        done(err);
+      });
+  });
+});
