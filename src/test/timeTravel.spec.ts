f1423ab5f74da4d83af227d21fa79688ae7fe713
jeonghoon <shb03323@naver.com>
2022-07-19 17:04:02 +0900

    [Bug] #46 - send 대신 field로 변경

--- src/test/timeTravel.spec.ts
+++ src/test/timeTravel.spec.ts
@@ -54,16 +54,17 @@ describe('POST /timeTravel', () => {
       .post('/timeTravel')
       .set('Content-Type', 'multipart/form-data')
       .set({ Authorization: `Bearer ${process.env.TEST_TOKEN}` })
-      .send({
-        title: '김루희 똥방구',
-        image: 'https://cdn.pixabay.com/photo/2022/04/29/17/48/lofoten-7164179_1280.jpg',
-        year: 2022,
-        month: 7,
-        day: 19,
-        currentDate: '2022.07.20',
-        questions: ['질문1', '질문2', '질문3', '질문4', '질문5', '질문6', '질문7'],
-        answers: ['대답1', '대답2', '대답3', '대답4', '대답5', '대답6', '대답7'],
-      })
+      .field("title", "김루희 똥방구")
+    //   .send({
+    //     "title": '김루희 똥방구',
+    //     "image": 'https://cdn.pixabay.com/photo/2022/04/29/17/48/lofoten-7164179_1280.jpg',
+    //     "year": 2022,
+    //     "month": 7,
+    //     "day": 19,
+    //     "currentDate": '2022.07.20',
+    //     "questions": ['질문1', '질문2', '질문3', '질문4', '질문5', '질문6', '질문7'],
+    //     "answers": ['대답1', '대답2', '대답3', '대답4', '대답5', '대답6', '대답7'],
+    //   })
       .expect(201)
       .then((res) => {
         done();
