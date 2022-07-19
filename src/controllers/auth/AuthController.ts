a57ac32953c858b39bb4a6cf220b7f3b55cd06b6
jeonghoon <shb03323@naver.com>
2022-07-19 16:00:17 +0900

    [Add] #44 - add logout null value case in controller

--- src/controllers/auth/AuthController.ts
+++ src/controllers/auth/AuthController.ts
@@ -61,6 +61,7 @@ const socialLogout = async (req: Request, res: Response, next: NextFunction) => 
   try {
     const data = await AuthService.socialLogout(userLogoutDto);
     if (!data) res.status(statusCode.NOT_FOUND).send(util.fail(statusCode.NOT_FOUND, message.NOT_FOUND));
+    if (data === exceptionMessage.NULL_VALUE) res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, message.NULL_VALUE));
     if (data === exceptionMessage.FCMTOKEN_INVALID) res.status(statusCode.NOT_FOUND).send(util.fail(statusCode.NOT_FOUND, message.INVALID_FCMTOKEN));
     res.status(statusCode.OK).send(util.success(statusCode.OK, message.LOGOUT_USER_SUCCESS, data));
   } catch (error) {
