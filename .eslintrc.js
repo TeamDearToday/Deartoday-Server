25e777b81d59b9425d0903c95c901f11df40403e
jeonghoon <shb03323@naver.com>
2022-07-21 16:43:01 +0900

    [Add] #81 - add husky

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,7 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
+    sourceType: 'module',
   },
   plugins: ['@typescript-eslint'],
   rules: {},
